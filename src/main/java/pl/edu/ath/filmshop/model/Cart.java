a5f8cdd60ff41f3af06c24b0ae9cf5df6d41e0bc
stamich <o4tgxzhz>
2019-07-14 11:23:08 +0200

    few changes

--- src/main/java/pl/edu/ath/filmshop/model/Cart.java
+++ src/main/java/pl/edu/ath/filmshop/model/Cart.java
@@ -1,7 +1,44 @@
 package pl.edu.ath.filmshop.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
 
 @Document
 public class Cart {
+
+    @Id
+    private Long id;
+
+    @Field
+    private Double totalCost;
+
+    @Field
+    private List<Film> films;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(Double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
 }
--- src/main/java/pl/edu/ath/filmshop/model/Film.java
+++ src/main/java/pl/edu/ath/filmshop/model/Film.java
@@ -1,7 +1,53 @@
 package pl.edu.ath.filmshop.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document
 public class Film {
+
+    @Id
+    private Long id;
+
+    @Field
+    private String title;
+
+    @Field
+    private Boolean availability;
+
+    @Field
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
